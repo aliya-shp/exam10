45e0a0ee63d6a703f9acc067588965fe40619d17
Aliya Akmatova <amavarati@gmail.com>
2020-02-22 14:07:42 +0600

    Added mysqlDB file with structure and no data yet

--- backend/app/news.js
+++ backend/app/news.js
@@ -27,27 +27,67 @@ router.get('/', async (req, res) => {
 router.post('/', upload.single('image'), async (req, res) => {
     const news = req.body;
 
-    console.log(news);
-
     if (req.file) {
         news.image = req.file.filename;
     }
 
-    console.log(news.image);
-
     const result = await newsDB.getConnection().query(
-        'INSERT INTO `news` (`title`, `text`, `image`, `datetime`) ' +
+        'INSERT INTO `equipment` (`title`, `text`, `image`, `datetime`) VALUES ' +
         '(?, ?, ?, ?)',
         [news.title, news.text, news.image, news.datetime]
     );
+
+    if (Object.values(news).length < 4) {
+        return res.status(400).send({message: 'You forgot to fill all the fields!'});
+    }
 
     news.id = result.insertId;
 
     res.send(news);
 });
 
-router.get('/:id', async (req, res) => {
+// router.post('/', upload.single('image'), async (req, res) => {
+//     const news = req.body;
+//
+//     console.log(news);
+//
+//     if (req.file) {
+//         news.image = req.file.filename;
+//     }
+//
+//     console.log(news.image);
+//
+//     const result = await newsDB.getConnection().query(
+//         'INSERT INTO `news` (`title`, `text`, `image`, `datetime`) ' +
+//         '(?, ?, ?, ?)',
+//         [news.title, news.text, news.image, news.datetime]
+//     );
+//
+//     news.id = result.insertId;
+//
+//     res.send(news);
+// });
 
+router.get('/:id', async (req, res) => {
+    const news = await newsDB.getConnection().query('SELECT * FROM `news` WHERE `id` = ?', req.params.id);
+
+    const newsItem = news[0];
+    if (!newsItem) {
+        return res.status(404).send({message: 'Was not found'});
+    }
+
+    res.send(news);
+});
+
+router.delete('/:id', async (req,res) => {
+    const news = await newsDB.getConnection().query('SELECT * FROM `locations` WHERE `id` = ?', req.params.id);
+
+    if (news.length === 0) {
+        res.status(400).send({message: 'No such news!'});
+    } else {
+        newsDB.getConnection().query('DELETE FROM `locations` WHERE `id` = ?', req.params.id);
+        res.send({message: 'Deleted news successfully'});
+    }
 });
 
 module.exports = router;
